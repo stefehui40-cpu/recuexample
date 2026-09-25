44c28cb3e16baee4f0f62d105f30a80c0724fca4
stefehui40-cpu <stefehui40@gmail.com>
2026-09-25 21:51:46 +0100

    Activer la nouvelle version sans attendre la fermeture des onglets

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='recu-mobile-v5';
+const CACHE='recu-mobile-v6';
 const FILES=['./','./index.html','./style.css','./app.js','./core.js','./pdf.js','./manifest.webmanifest','./template.png','./ReceiptSans.ttf','./icon.svg','./icon-192.png','./icon-512.png','./pdf-lib.min.js','./fontkit.umd.min.js','./qrcode.js'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES.map(path=>new Request(path,{cache:'reload'})))).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('recu-mobile-')&&key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;

0b6a6e4eb28a5b75a1cce764db2cbcc0b10240b6
stefehui40-cpu <stefehui40@gmail.com>
2026-09-25 21:34:10 +0100

    Actualiser la version hors connexion avec la saisie rapide

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='recu-mobile-v4';
+const CACHE='recu-mobile-v5';
 const FILES=['./','./index.html','./style.css','./app.js','./core.js','./pdf.js','./manifest.webmanifest','./template.png','./ReceiptSans.ttf','./icon.svg','./icon-192.png','./icon-512.png','./pdf-lib.min.js','./fontkit.umd.min.js','./qrcode.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('recu-mobile-')&&key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
@@ -7,8 +7,5 @@ self.addEventListener('fetch',event=>{
   // Cache only the application files, never a generated PDF or patient data.
   event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
 });
-
-
-
 
 

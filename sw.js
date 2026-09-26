3ff3409550e20cc3037ab1d8006de82408990b59
stefehui40-cpu <stefehui40@gmail.com>
2026-09-26 10:01:41 +0100

    Actualiser le cache pour la hauteur uniforme des chiffres

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='recu-mobile-v11';
+const CACHE='recu-mobile-v12';
 const FILES=['./','./index.html','./style.css','./app.js','./core.js','./pdf.js','./manifest.webmanifest','./template.png','./ReceiptSans.ttf','./icon.svg','./icon-192.png','./icon-512.png','./pdf-lib.min.js','./fontkit.umd.min.js','./qrcode.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES.map(path=>new Request(path,{cache:'reload'})))).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('recu-mobile-')&&key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
